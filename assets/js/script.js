df07aaf60d582fa28e729d753cd4b94ffba6f741
KimboSlice <kimberlyhoward529@gmail.com>
2022-06-24 15:35:14 -0400

    commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,86 +12,83 @@ function generatePassword() {
 
   // Prompt user to enter password length
   var passwordLength = window.prompt("Enter the number of characters you would like in your password. (Must be between 8 and 128 characters.");
+
   // Validate that user input is a number between 8 and 128
   if (passwordLength > 7 && passwordLength < 129) {
   } else {
-    window.confirm("Please enter a number between 8 and 128.");
+    window.confirm("Please try again. Next time enter a number between 8 and 128.");
+    return;
   }
 
   // Set user input as passwordArray length
   passwordArray.length = passwordLength;
 
-  // Asks user what characters they want to be in password
+  // Describe next steps of process and tell user they must select OK to at least one of the specifications
   window.alert("Next you will choose the specifications for your password. Select OK on at least one of the following.");
+
+  // Ask user if they want lower case letters
   var passwordLowerCase = window.confirm("Select OK if you would like your password to contain lower case letters.");
 
+  // Ask user if they want upper case letters
   var passwordUpperCase = window.confirm("Select OK if you would like your password to contain upper case letters");
     
+  // Ask user if they want special characters
   var passwordSpecial = window.confirm("Select OK if you would like your password to contain special characters");
 
+  // Ask user if they want numbers
   var passwordNumbers = window.confirm("Select OK if you would like your password to contain numbers");
  
 
-  // Step 2: Generate the password based on the criteria
-  // a) Create 4 arrays that hold possible lower case, upper case, and special characters and numbers
+  // Generate the password based on the criteria
+  // Create 4 arrays that hold possible lower case, upper case, and special characters and numbers
   var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
   var specailChar = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')'];
   var number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-  // var allCharacters = lowerCase.concat(upperCase, specailChar, number);
 
+  // If user selects OK for lower case letters, it adds the array of lower case letters to the array that contains possible characters for password
   if (passwordLowerCase) {
     babySelection = lowerCase;
-    // console.log("Your password may contain lower case letters.");
   } else {
-      // console.log("Your password will not conatin lower case letters");
+
   }
 
+  // If user selects OK for uppper case letters, it adds the array of upper case letters to the array that contains possible characters for password
   if (passwordUpperCase) {
     babySelection = babySelection.concat(upperCase);
-    // console.log("Your password may contain upper case letters.");
+    
   } else {
-      // console.log("Your password will not conatin upper case letters");
+      
   }
 
+  // If user selects OK for special characters, it adds the array of special characters to the array that contains possible characters for password
   if (passwordSpecial) {
     babySelection = babySelection.concat(specailChar);
-    // console.log("Your password may contain special characters.");
-    // console.log(babySelection);
+    
   } else {
-      // console.log("Your password will not conatin special characters");
-      // console.log(babySelection);
+      
   }
 
+  // If user selects OK for numbers, it adds the array of numbers to the array that contains possible characters for password
   if (passwordNumbers) {
     babySelection = babySelection.concat(number);
-    // console.log("Your password may contain numbers.");
-    // console.log(babySelection);
+    
   } else {
-      // console.log("Your password will not conatin numbers");
-      // console.log(babySelection);
+  
   }
-
-  // var index = Math.floor(Math.random() * babySelection.length);
-  // console.log(index);
-
-  // console.log(babySelection);  
+ 
+  // Selects random characters that will be characters in password
   for (var i = 0; i < parseInt(passwordLength); i++) {
     var index = Math.floor(Math.random() * (babySelection.length + 1));
     console.log(index);
     passwordArray[i] = babySelection[index];
   }
 
-  console.log(passwordArray);
-
+  // Creates a variable to hold a string that contains the characters from the password array
   var passwordString = passwordArray.join("");
-  console.log(passwordString);
+  
 
-  // for (var i = 0; i < passwordLength; i++) {
-  //   if(password)
-  // }
-
-  // Step 4: Display password to the page
+  // Gives the variable that is the string for the password so it displays onto the page
   return passwordString;
 }
 
@@ -103,6 +100,4 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
-// Add event listener to generate button
 
